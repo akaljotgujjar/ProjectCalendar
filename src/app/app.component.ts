4260161f712a64e3fb3e0a64492d111487b22080
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-10-31 14:35:33 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { View } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<ejs-schedule height="850" width="1250" [currentView]="setView"></ejs-schedule>',
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ProjectCalendar';
+  public setView: View = 'Month';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// tslint:disable-next-line: max-line-length
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScheduleModule, RecurrenceEditorModule
   ],
-  providers: [],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
